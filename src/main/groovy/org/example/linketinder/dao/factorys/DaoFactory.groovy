6e3c2f061e89b7877e7452fd851b35db79838f00
adrielly <mendes.silva.vitoria.adrielly@gmail.com>
2024-04-20 15:04:04 -0300

    criando só um arquivo para todos  os factory

---
+++ src/main/groovy/org/example/linketinder/dao/factorys/DaoFactory.groovy
@@ -0,0 +1,58 @@
+package org.example.linketinder.dao.factorys
+
+import org.example.linketinder.dao.implementacoes.CandidatoCompetenciaDaoImpl
+import org.example.linketinder.dao.implementacoes.CandidatoDaoImpl
+import org.example.linketinder.dao.implementacoes.CandidatoVagaDaoImpl
+import org.example.linketinder.dao.implementacoes.CompetenciaDaoImpl
+import org.example.linketinder.dao.implementacoes.EmpresaDaoImpl
+import org.example.linketinder.dao.implementacoes.VagaCompetenciaDaoImpl
+import org.example.linketinder.dao.implementacoes.VagaDaoImpl
+import org.example.linketinder.dao.interfaces.CandidatoCompetenciaDao
+import org.example.linketinder.dao.interfaces.CandidatoDao
+import org.example.linketinder.dao.interfaces.CandidatoVagaDao
+import org.example.linketinder.dao.interfaces.CompetenciaDao
+import org.example.linketinder.dao.interfaces.EmpresaDao
+import org.example.linketinder.dao.interfaces.VagaCompetenciaDao
+import org.example.linketinder.dao.interfaces.VagaDao
+import org.example.linketinder.database.ConectarBanco
+
+class DaoFactory {
+    static CandidatoCompetenciaDao criarInstanciaCandidatoCompetencia() {
+        ConectarBanco conectarBanco = ConectarBanco.criarInstancia()
+        return new CandidatoCompetenciaDaoImpl(conectarBanco)
+    }
+
+    static CandidatoDao criarInstanciaCandidato() {
+        ConectarBanco conectarBanco = ConectarBanco.criarInstancia()
+        CandidatoCompetenciaDao candidatoCompetenciaDao = criarInstanciaCandidatoCompetencia()
+        return new CandidatoDaoImpl(conectarBanco, candidatoCompetenciaDao)
+    }
+
+    static CandidatoVagaDao criarInstanciaCandidatoVaga() {
+        ConectarBanco conectarBanco = ConectarBanco.criarInstancia()
+        VagaCompetenciaDao vagaCompetenciaDao = criarInstanciaVagaCompetencia()
+
+        return new CandidatoVagaDaoImpl(conectarBanco, vagaCompetenciaDao)
+    }
+
+    static CompetenciaDao criarInstanciaCompetencia() {
+        ConectarBanco conectarBanco = ConectarBanco.criarInstancia()
+        return new CompetenciaDaoImpl(conectarBanco)
+    }
+
+    static EmpresaDao criarInstanciaEmpresa() {
+        ConectarBanco conectarBanco = ConectarBanco.criarInstancia()
+        return new EmpresaDaoImpl(conectarBanco)
+    }
+
+    static VagaCompetenciaDao criarInstanciaVagaCompetencia() {
+        ConectarBanco conectarBanco = ConectarBanco.criarInstancia()
+        return new VagaCompetenciaDaoImpl(conectarBanco)
+    }
+
+    static VagaDao criarInstanciaVaga() {
+        ConectarBanco conectarBanco = ConectarBanco.criarInstancia()
+        VagaCompetenciaDao vagaCompetenciaDao = criarInstanciaVagaCompetencia()
+        return new VagaDaoImpl(conectarBanco, vagaCompetenciaDao)
+    }
+}
